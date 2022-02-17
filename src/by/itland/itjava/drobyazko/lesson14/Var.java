f0311a521938119f7c395a6464ec2e3688af7012
Sasha <sasha.drobyazko.17@gmail.com>
2022-02-17 21:03:46 +0300

    drobyazko.lesson14

--- src/by/itland/itjava/drobyazko/lesson14/Var.java
+++ src/by/itland/itjava/drobyazko/lesson14/Var.java
@@ -3,25 +3,29 @@ package by.itland.itjava.drobyazko.lesson14;
 public abstract class Var implements Operation{
     @Override
     public Var add(Var other) {
-        System.out.println("Error");
+        System.out.println("Error: сложение" + this.getClass().getSimpleName() + "невозможна с" +
+                other.getClass().getSimpleName());
         return null;
     }
 
     @Override
     public Var sub(Var other) {
-        System.out.println("Error");
+        System.out.println("Error: вычитание" + this.getClass().getSimpleName() + "невозможна с" +
+                other.getClass().getSimpleName());
         return null;
     }
 
     @Override
     public Var mull(Var other) {
-        System.out.println("Error");
+        System.out.println("Error: произведение" + this.getClass().getSimpleName() + "невозможна с" +
+                other.getClass().getSimpleName());
         return null;
     }
 
     @Override
     public Var div(Var other) {
-        System.out.println("Error");
+        System.out.println("Error: частное" + this.getClass().getSimpleName() + "невозможна с" +
+                other.getClass().getSimpleName());
         return null;
     }
 
